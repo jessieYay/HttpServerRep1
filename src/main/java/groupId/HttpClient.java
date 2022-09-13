970c34a626a6a77e254b7fe38de288c330399c46
jessieYay <99616303+jessieYay@users.noreply.github.com>
2022-09-13 14:00:35 +0200

    Add HttpClient Class

--- src/main/java/groupId/HttpClient.java
+++ src/main/java/groupId/HttpClient.java
@@ -9,6 +9,7 @@ public class HttpClient {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
         Socket socket = new Socket("httpbin.org", 80);
+        String hei;
         String request = ("GET / HTTP/1.1\n" +
                 "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n" +
                 "Accept-Language: nb-NO,nb;q=0.9,no;q=0.8,nn;q=0.7,en-US;q=0.6,en;q=0.5\n" +
