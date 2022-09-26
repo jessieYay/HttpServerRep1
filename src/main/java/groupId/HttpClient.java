ef166157bd0bc4922d02b9431039873e64ef2ef8
jessieYay <99616303+jessieYay@users.noreply.github.com>
2022-09-26 13:52:35 +0200

    🔴 added test for status code 200

--- src/main/java/groupId/HttpClient.java
+++ src/main/java/groupId/HttpClient.java
@@ -6,18 +6,35 @@ import java.nio.charset.StandardCharsets;
 
 public class HttpClient {
 
+    public HttpClient(String host, int port, String requestTarget) {
+
+    }
+
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
         Socket socket = new Socket("httpbin.org", 80);
         String hei;
-        String request = ("GET / HTTP/1.1\n" +
-                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n" +
-                "Accept-Language: nb-NO,nb;q=0.9,no;q=0.8,nn;q=0.7,en-US;q=0.6,en;q=0.5\n" +
-                "Cache-Control: max-age=0\n" +
-                "Connection: keep-alive\n" +
-                "Host: httpbin.org\n" +
-                "Upgrade-Insecure-Requests: 1\n" +
-                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
-        socket.getOutputStream().write(request.getBytes(StandardCharsets.UTF_8));
+        // alt + j kan man markere alle lignende ord eller betegnelser og markere dem.
+        String request = "GET /html HTTP/1.1\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n" +
+                "Accept-Language: nb-NO,nb;q=0.9,no;q=0.8,nn;q=0.7,en-US;q=0.6,en;q=0.5\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "Connection: close\r\n" +
+                "Host: httpbin.org\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36\r\n" +
+                "\r\n";
+        socket.getOutputStream().write(request.getBytes(StandardCharsets.UTF_8));// lim in i getbytes
+
+        int c;
+        while((c = socket.getInputStream().read()) != -1 ){
+            System.out.print((char)c);
+
+        }
+    }
+
+    public int getStatusCode() {
+
+        return 0;
     }
 }
--- src/test/java/groupId/HttpClientTest.java
+++ src/test/java/groupId/HttpClientTest.java
@@ -2,11 +2,19 @@ package groupId;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HttpClientTest {
 
-    //@Test
+    @Test
 
-    //void httpTestOne(){
+    void shouldGetStatusCodeOk(){
+
+        var client = new HttpClient("httpbin.org",80,"/html");
+
+        assertEquals(200, client.getStatusCode());
+    }
+
 
 
 
