1f9324db094934f0094edd2a5587a23ca2844dbd
jessieYay <99616303+jessieYay@users.noreply.github.com>
2022-10-02 20:38:00 +0200

    fix Http server main method

--- src/main/java/groupId/HttpServer.java
+++ src/main/java/groupId/HttpServer.java
@@ -49,28 +49,9 @@ public class HttpServer {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
 
-        var serverSocket = new ServerSocket(9080);
+        var server = new HttpServer(9080);
 
-        var clientSocket = serverSocket.accept();
-
-
-
-
-        var body = "<html><h1>Hello world and Jessie!</h1></html>";
-        var contentLength = body.getBytes().length;
-
-        clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
-                "Date: Wed, 28 Sep 2022 13:23:50 GMT\r\n" +
-                "Content-Type: text/html; charset=utf-8\r\n" +
-                "Content-Length " + contentLength + "\r\n" +
-                "Connection: keep-alive\r\n" +
-                "\r\n" +
-                body ).getBytes(StandardCharsets.UTF_8));
-
-        // text/html; charset=utf-8
-        // text/plain;
 
     }
 
